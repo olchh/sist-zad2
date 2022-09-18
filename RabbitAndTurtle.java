9e1106c2154534ea33a7b01c02b7af280c3d1764
olchh <98604592+olchh@users.noreply.github.com>
2022-09-18 22:46:37 +0800

    Add files via upload

---
+++ RabbitAndTurtle.java
@@ -0,0 +1,29 @@
+package com.company;
+
+class AnimalThread extends Thread {
+    AnimalThread(String name,int prioritet) {
+        this.name = name;
+        this.prioritet = prioritet;
+    }
+    int prioritet;
+    String name;
+
+    public void run() {
+        Thread theard = new Thread();
+        for(int i = 0; i <= 1000; i += 100){
+            System.out.println( name + " пробежал(а): " + i + " метров");
+            if(i == 500) {
+                prioritet = MIN_PRIORITY;
+                theard.setPriority(prioritet); //замена приоритета
+            }
+        }
+    }
+}
+public class RabbitAndTurtle{
+    public static void main(String args[]){
+        AnimalThread rabbit = new AnimalThread("Кролик",Thread.NORM_PRIORITY);
+        rabbit.start();
+        AnimalThread turtle = new AnimalThread("Черепаха",Thread.NORM_PRIORITY);
+        turtle.start();
+    }
+}
